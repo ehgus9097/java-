db7f5ceecdbcf423ca850fe9a16c2fd090ce5ead
DoHyoun <ehgus9097@naver.com>
2022-11-18 14:19:57 +0900

    백준 문제풀이 사칙연산

---
+++ 1단계 입출력,사칙연산/baekjoon1000.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class baekjoon1000 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
